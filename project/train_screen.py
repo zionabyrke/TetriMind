c0207c979f6770079cff5fd4a610f498157f222c
zionabyrke <renzkirbyonia@gmail.com>
2025-12-07 04:13:44 +0800

    kalimutan

--- project/train_screen.py
+++ project/train_screen.py
@@ -1,14 +1,9 @@
 ### RUN THIS SCRIPT FOR TRAINING ONLY"""" 
 from settings import *
-from game import Playfield, GameInfo
+from game import Game, Bag, Tetromino
 from agent import Agent
-from gen_algo import RLGenAlgo
 
-import json # model saving
-# load model
-model_weights = None
-with open("rlga_v1.json", "r") as f:
-    model_weights = json.load(f)
+testing_seed = 5
 
 pygame.init()
 screen = pygame.display.set_mode((GAME_WIDTH+RIGHTBAR_WIDTH+PADDING*3, GAME_HEIGHT+APPNAME_SIZE+PADDING*2))
@@ -16,136 +11,155 @@ pygame.display.set_caption("Training Arc")
 clock = pygame.time.Clock()
 
 #objects
-info = GameInfo()
-field = Playfield(info)
-info.field = field 
-agent = Agent(info)
-GA = RLGenAlgo(info, generations=20, population_size=12)
+bag = Bag(testing_seed)
+game = Game(bag)
+agent = Agent(game)
 
-colorMatrix = [[BLACK for _ in range(COLUMNS)] for _ in range(ROWS)]
+### TESTING ###
+# T spin check
+#game.current_piece = Tetromino("Z")
+game.current_piece = Tetromino("T")
+game.block_matrix = [
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,1,1,1],
+        [1,1,1,1,1,0,0,0,1,1],
+        [1,1,1,1,1,1,0,1,1,1]
+        ]
+
+color_matrix = [[BLACK for _ in range(COLUMNS)] for _ in range(ROWS)]
 font_title = pygame.font.SysFont("consolas", APPNAME_SIZE)
 font_small = pygame.font.SysFont("consolas", 14)
+
+### testing color matrix ###
+for y in range(ROWS):
+    for x in range(COLUMNS):
+        if game.block_matrix[y][x] == 1:
+            color_matrix[y][x] = RED
 
 #surfaces
 playfield_surface = pygame.Surface((GAME_WIDTH, GAME_HEIGHT))
 sidebar_surface = pygame.Surface((RIGHTBAR_WIDTH, GAME_HEIGHT))
 
-piece_per_second = 1
 move_time = 0
-cleared = 0
+action_sequence=0
+action=None
 
-##### GA GENERATION LOOP
-for g in range(GA.generations):
-    best_score, best_weights = GA.run_generation()
-    best_final = GA.get_best_final()
+### game loop
+running = True
+while running:
+    dt = clock.tick(FRAMEPERSEC)
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-    # reset components for each generation
-    info = GameInfo()
-    field = Playfield(info)
-    info.field = field
-    agent = Agent(info)
+    #game logic
+    game.update(dt, color_matrix)
 
-    # set weights
-    agent.set_eval_function(lambda f: GA.evaluate_field(f, model_weights))
-
-    ### game loop
-    running = True
-    pieces = 0
-    move_time = 0
-    colorMatrix = [[BLACK for _ in range(COLUMNS)] for _ in range(ROWS)]
-    while running:
-        dt = clock.tick(FRAMEPERSEC)
-        
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-                pygame.quit()
-                quit()
-
-        field.update(dt, colorMatrix)
-        info.updateGameInfo(dt)
-
-        if field.game_over:
-            #new generations
-            break
-
-        # stop when piece limit is reached
-        if pieces >= 40: # should same on RLGenAlgo.evaluate_fitness()'s max_piece=40
-            break
-
-        h, b, colHeights = agent.getGameState()  # game states
-        temp = " ".join(map(str, colHeights)) #no space and brackets
-
-        ######   agent actions HERE
-        if(move_time <= piece_per_second):
+    h, b, col_heights = agent.get_game_states()  # game states
+    temp = " ".join(map(str, col_heights)) #no space and brackets
+    
+    ######   agent actions HERE
+    if action:
+        # do action in order
+        if(move_time <= agent.move_per_sec):
             move_time += dt/1000
         else:
-            move_time = 0   # reset move time
-            _, action = agent.chooseAction(field, depth=2) # recursion = piece lookahead
-            field.moveTetromino(action[0], colorMatrix)
-            for dx in range(abs(action[1])):
-                if action[1] < 0:
-                    field.moveTetromino(MOVE_LEFT, colorMatrix)
-                else:
-                    field.moveTetromino(MOVE_RIGHT, colorMatrix)
-            field.moveTetromino(action[2], colorMatrix)
-            pieces += 1  
+            # resets the move time
+            move_time = 0
+            # first action sequence - rotates tetromino to initial rotation
+            if action_sequence==0:
+                for rot in range(0 + action[0]):
+                    game.move_tetromino(ROTATE_RIGHT, color_matrix)
+                action_sequence += 1
+            # second action sequence - moves tetromino to drop x coordinate move by move, not instant
+            elif action_sequence==1:
+                dx = action[1] - game.current_piece.coord[0]
+                if dx < 0:
+                    game.move_tetromino(MOVE_LEFT, color_matrix)
+                    dx -= 1
+                elif dx > 0:
+                    game.move_tetromino(MOVE_RIGHT, color_matrix)
+                    dx += 1
+                if dx == 0:
+                    action_sequence += 1
+            # third action sequence - soft drop the tetromino
+            elif action_sequence==2:
+                game.soft_drop()
+                action_sequence+=1
+            # fourth action sequence - if we have a t-spin, then we rotate the tetromino when it's in the bottom
+            elif action_sequence==3:
+                game.move_tetromino(action[2], color_matrix)
+                action_sequence+=1
+            # finally we let the game place the block, then we clear action tuple and reset sequence
+            elif action_sequence==4:
+                game.update(game.fall_speed*1000+1, color_matrix)
+                action=None
+                action_sequence=0
+    else:
+        action = agent.choose_action(game)
 
-        screen.fill(GRAY)
-        playfield_surface.fill(BLACK)
-        sidebar_surface.fill(BLACK)
+    screen.fill(GRAY)
+    playfield_surface.fill(BLACK)
+    sidebar_surface.fill(BLACK)
 
-        # playfield blocks
-        for y, row in enumerate(colorMatrix):
-            for x, color in enumerate(row):
-                if color:
-                    pygame.draw.rect(playfield_surface, color,
-                                    (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+    # playfield blocks
+    for y, row in enumerate(color_matrix):
+        for x, color in enumerate(row):
+            if color:
+                pygame.draw.rect(playfield_surface, color,
+                                 (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE))
 
-        # piece and preview only, no ghost piece
-        if field.currentPiece:
-            shape_current = field.currentPiece.getShapeArray()
-            shape_next = field.nextPiece.getShapeArray()
-            preview_cell = CELL_SIZE // 1.5  # smaller display
+    # piece and preview only, no ghost piece
+    if game.current_piece:
+        shape_current = game.current_piece.get_shape_array()
+        shape_next = game.next_piece.get_shape_array()
+        preview_cell = CELL_SIZE // 1.5  # smaller display
 
-            for dx, dy in shape_current:
-                px, py = (field.currentPiece.coord[0] + dx) * CELL_SIZE, (field.currentPiece.coord[1] + dy) * CELL_SIZE
-                pygame.draw.rect(playfield_surface, field.currentPiece.color, (px, py, CELL_SIZE, CELL_SIZE))
+        for dx, dy in shape_current:
+            px, py = (game.current_piece.coord[0] + dx) * CELL_SIZE, (game.current_piece.coord[1] + dy) * CELL_SIZE
+            pygame.draw.rect(playfield_surface, game.current_piece.color, (px, py, CELL_SIZE, CELL_SIZE))
 
-            for px, py in shape_next:
-                # Draw the next piece preview
-                pygame.draw.rect(sidebar_surface, field.nextPiece.color, 
-                (PADDING+30+px * preview_cell, PADDING+70+py * preview_cell, preview_cell, preview_cell))
+        for px, py in shape_next:
+            # Draw the next piece preview
+            pygame.draw.rect(sidebar_surface, game.next_piece.color, 
+            (PADDING+30+px * preview_cell, PADDING+70+py * preview_cell, preview_cell, preview_cell))
 
-        # sidebar
-        title_text = font_title.render("TETRIMIND", True, LINE_COLOR)
-        score_text = font_small.render(f"Score: {info.playerScore}", True, LINE_COLOR)
-        level_text = font_small.render(f"Level: {info.gameLevel}", True, LINE_COLOR)
-        preview_text = font_small.render(f"Next Piece:", True, LINE_COLOR)
-        states_text = [f"Holes: {h}", f"Bumpiness: {b}", "Heights:", f"{temp}"]
-        for x, text in enumerate(states_text):
-            state_text = font_small.render(text, True, LINE_COLOR)
-            sidebar_surface.blit(state_text, (PADDING, PADDING+150+(x*20)))
 
-        ga_y = PADDING + 150 + len(states_text) * 20 + 20  # position below states_text
-        gen_text = font_small.render(f"GENERATION: {g+1}", True, LINE_COLOR)
-        sidebar_surface.blit(gen_text, (PADDING, ga_y))
-        ga_y += 20
+    # sidebar
+    title_text = font_title.render("TETRIMIND", True, LINE_COLOR)
+    score_text = font_small.render(f"Score: {game.player_score}", True, LINE_COLOR)
+    level_text = font_small.render(f"Level: {game.game_level}", True, LINE_COLOR)
+    preview_text = font_small.render(f"Next Piece:", True, LINE_COLOR)
+    states_text = [f"Holes: {h}", f"Bumpiness: {b}", "Heights:", f"{temp}", 
+        f"Lines cleared: {game.lines_cleared_so_far}"]
+    for x, text in enumerate(states_text):
+        state_text = font_small.render(text, True, LINE_COLOR)
+        sidebar_surface.blit(state_text, (PADDING, PADDING+150+(x*20)))
 
-        cleared_text = font_small.render(f"LINES CLEARED: {field.lines_cleared_so_far}", True, LINE_COLOR)
-        sidebar_surface.blit(cleared_text, (PADDING, ga_y))
-        ga_y += 20
-        
-        sidebar_surface.blit(score_text, (PADDING, PADDING))
-        sidebar_surface.blit(level_text, (PADDING, PADDING+16))
-        sidebar_surface.blit(preview_text, (PADDING, PADDING+16+16))
-        screen.blit(playfield_surface, (PADDING, PADDING + APPNAME_SIZE))
-        screen.blit(sidebar_surface, (GAME_WIDTH + PADDING * 2, PADDING + APPNAME_SIZE))
-        screen.blit(title_text, (PADDING, PADDING))
+    sidebar_surface.blit(score_text, (PADDING, PADDING))
+    sidebar_surface.blit(level_text, (PADDING, PADDING+16))
+    sidebar_surface.blit(preview_text, (PADDING, PADDING+16+16))
+    screen.blit(playfield_surface, (PADDING, PADDING + APPNAME_SIZE))
+    screen.blit(sidebar_surface, (GAME_WIDTH + PADDING * 2, PADDING + APPNAME_SIZE))
+    screen.blit(title_text, (PADDING, PADDING))
 
-        pygame.display.update()
+    pygame.display.update()
 
-    # save model
-    with open("rlga_v1.json", "w") as f:
-        json.dump(best_final, f, indent=4)
-    
+pygame.quit()
